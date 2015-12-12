0fd29a44888ae26ccb08f0a765e4690ddd355eaa
Ainul Yaqin <bufferoverflow000@gmail.com>
2015-12-12 11:06:42 +0700

    Menambah Method Tambah Tambah Siswa Kelas

--- app/Models/m_wali.php
+++ app/Models/m_wali.php
@@ -4,6 +4,7 @@ use Resources;
  
 class M_wali {
     public function __construct(){
+        $this->session = new Resources\Session;
         $this->db = new Resources\Database;
     }
     
@@ -12,8 +13,13 @@ class M_wali {
     	Return $result = $this->db->results("SELECT * FROM table_siswa ORDER BY ID_SISWA ASC LIMIT $offset, $limit");
     }
     
-    public function tambahsiswakelas() {
-            
+    public function tambahsiswakelas($value) {
+        Return $result = $this->db->insert("table_siswa", $value);
+    }
+    
+    public function query($wali) {
+        Return $this->db->row("SELECT ID_KELAS FROM table_wali_kelas AS wali INNER JOIN table_guru_mapel AS guru "
+                . "ON wali.ID_WALI = guru.ID_GURU WHERE wali.ID_WALI = $wali");
     }
     
     public function editdatasiswa() {
