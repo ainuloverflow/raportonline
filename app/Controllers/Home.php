7d3206d6a0acb70690d83afb78dc055e2cd22e8d
Ainul Yaqin <bufferoverflow000@gmail.com>
2016-06-12 21:57:06 +0700

    this is

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -15,7 +15,6 @@ class Home extends Resources\Controller
 	
     public function direct() {
         $this->redirect('login'); //asdfasdf
-        $this->redirect('login');
     }
 	
     public function login() {
